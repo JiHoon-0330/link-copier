54ec7bb4211e18833048deacbde2f91dde4e1532
JiHoon-0330 <ljihoon0330@gmail.com>
2025-06-27 15:09:17 +0900

    content 스크립트 추가 및 관련 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,20 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import path from "node:path";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+        content: path.resolve(__dirname, "scripts/content.ts"),
+      },
+      output: {
+        entryFileNames: "[name].js",
+        assetFileNames: "[name].[ext]",
+      },
+    },
+  },
+});

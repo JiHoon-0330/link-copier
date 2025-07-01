e9d07492806df5b45deebe34cf2df1854b3e3be9
JiHoon-0330 <ljihoon0330@gmail.com>
2025-07-01 16:49:52 +0900

    esbuild 설정 변경

--- esbuild.scripts.ts
+++ esbuild.scripts.ts
@@ -1,19 +1,11 @@
-import { resolve } from "node:path";
 import { build } from "esbuild";
 
-async function buildScripts(path: string) {
-  const filePath = resolve(__dirname, path);
-  await build({
-    entryPoints: [filePath],
-    bundle: true,
-    minify: true,
-    outdir: "./dist",
-  });
-  console.log(`${path} compiled`);
-}
-
-async function bundle(...pathList: string[]) {
-  await Promise.all(pathList.map((path) => buildScripts(path)));
-}
-
-await bundle("scripts/content.ts", "scripts/service-worker.ts");
+await build({
+  entryPoints: {
+    content: "./scripts/content.ts",
+    "service-worker": "./scripts/service-worker.ts",
+  },
+  bundle: true,
+  minify: true,
+  outdir: "./dist",
+});
